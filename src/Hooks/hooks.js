aa8ac82abe41e80a5a61450bfc626fcce05e729d
sazzad <sazzadkarim01@gmail.com>
2022-05-25 19:51:27 +0600

    hooks part commit

--- src/Hooks/hooks.js
+++ src/Hooks/hooks.js
@@ -1,21 +1,23 @@
-import React from "react";
-import banner from "../../assets/image/banner.jpg";
+import { useEffect, useState } from "react";
 
-const Banner = () => {
-  return (
-    <div className="flex justify-around items-center mt-12 h-[50vh]">
-      <div className="flex-1">
-        <img src={banner} alt="laptopImage" />
-      </div>
-      <div className="flex-1 lg:leading-10">
-        <h1 className="text-3xl font-semibold lg:leading-10">
-          Stored your honorable<span className="text-orange-600"> laptop</span>
-        </h1>
-        <h1 className="text-3xl font-semibold lg:leading-10">Because it's a storing website!</h1>
-        <button className="btn ring-2 rounded-3xl px-6 mt-3 ring-green-300 font-bold hover:bg-green-200">Store now</button>
-      </div>
-    </div>
-  );
-};
-
-export default Banner;
+const useToken = user => {
+    const [token, setToken] = useState('')
+    useEffect(() => {
+        const email = user?.email;
+        const currentUser = email;
+        if (currentUser) {
+            const url = `http://localhost:5000/user/${email}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify('email')
+            })
+                .then(res => res.json())
+                .then(data => console.log(data));
+        }
+    }, [user])
+    return [token];
+}
+export default useToken;
